f585f5bd63171eb1a9fb063798d236ec059e942a
FJVicVel03 <3021894750101@ingenieria.usac.edu.gt>
2025-08-16 19:12:31 -0600

    feat: API básica con mi nombre y canción

---
+++ Tarea1/api.py
@@ -0,0 +1,15 @@
+###Api Rest con flask básica con mi nombre y canción favorita
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def index():
+    ##retoonrar mi nombre y canción favorita
+    return jsonify({
+        'Nombre': 'Fernando José Vicente Velásquez',
+        'Canción Fav': 'Zinni e Buotti'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
+# Para ejecutar la API, guarda este código en un archivo llamado api.py y ejecuta el comando:
+# python api.py
