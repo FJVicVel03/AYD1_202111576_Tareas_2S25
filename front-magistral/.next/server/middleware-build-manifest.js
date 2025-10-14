5a186a742d16e0e0e9cbe8b7aa54a25d6c239bcc
Paola Alonzo <pao.alonzo31452@gmail.com>
2025-10-14 13:43:24 -0600

    feat: cambios

--- front-magistral/.next/server/middleware-build-manifest.js
+++ front-magistral/.next/server/middleware-build-manifest.js
@@ -24,10 +24,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/colaboracion": [
+    "/mockups": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/colaboracion.js"
+      "static/chunks/pages/mockups.js"
     ]
   },
   "ampFirstPages": []
